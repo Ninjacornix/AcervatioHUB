ad4e179232f1fb79e8f051cbb549a7845a225102
Ninjacornix <milanvrankic@gmail.com>
2019-11-28 16:50:50 +0100

    Initialized todo

---
+++ src/todo/main.js
@@ -0,0 +1,13 @@
+// declaring variables
+const todo = document.createElement("div");
+const todoDiv = document.createElement("div");
+const inProgress = document.createElement("div");
+const done = document.createElement("div");
+
+// style 
+
+// appending vars
+todo.appendChild(todoDiv);
+todo.appendChild(inProgress);
+todo.appendChild(done);
+document.style.appendChild(todo);
