7c583160572bf67406265306a1bd612d7d11de88
Ninjacornix <milanvrankic@gmail.com>
2019-12-05 22:14:00 +0100

    fixed a function

--- src/todo/main.js
+++ src/todo/main.js
@@ -5,17 +5,22 @@ const inProgress = document.createElement("div");
 const done = document.createElement("div");
 
 // style
+todo.style.backgroundColor = "white";
+inProgress.style.backgroundColor = "white";
+done.style.backgroundColor = "white";
+
 
 // appending vars
 todo.appendChild(todoDiv);
 todo.appendChild(inProgress);
 todo.appendChild(done);
-document.style.appendChild(todo);
+document.body.appendChild(todo);
 
 // events
 
 // functions
 function additem(){
-    document.createElement("div");
-    
+    const addtotodo = document.createElement("div");
+    addtotodo.innerHTML = "hi";
+    done.appendChild(addtotodo);
 }
