9f616a626bf5d3e720ebe046cb1372afe2b6d85b
Ninjacornix <milanvrankic@gmail.com>
2019-12-22 23:33:47 +0100

    worked on todo functionality

--- src/todo/main.js
+++ src/todo/main.js
@@ -2,9 +2,11 @@ const input = document.getElementById("input");
 const button = document.getElementById("btn");
 const todolist = document.getElementById("todo_items");
 const donelist = document.getElementById("done_items");
+const check = document.getElementById("check");
+const icon = document.getElementById("icon");
 
 button.addEventListener("click", addFunc);
-todolist.addEventListener("click", moveItem);
+/*todolist.addEventListener("click", moveItem); */
 
 function addFunc(event){
     event.preventDefault();
@@ -40,6 +42,34 @@ function moveItem(event){
     }
 }
 
+let ifchecked = 0;
+let trigger = 0;
 function markasdone(){
+    if(ifchecked === 0){
+        check.removeChild(check.firstElementChild);
+        const newicon = document.createElement("i");
+        newicon.setAttribute("id", "icon");
+        newicon.setAttribute("onclick","markasdone()");
+        newicon.setAttribute("class", "fa fa-times my-float");
+        check.appendChild(newicon);
+        check.style.backgroundColor = "white";
+        ifchecked = 1;
+        trigger = 1;
+    } else if(ifchecked === 1){
+        check.removeChild(check.firstElementChild);
+        const newicon = document.createElement("i");
+        newicon.setAttribute("id", "icon");
+        newicon.setAttribute("onclick","markasdone()");
+        newicon.setAttribute("class", "fa fa-check my-float");
+        check.appendChild(newicon);
+        check.style.backgroundColor = "#6C757D";
+        ifchecked = 0; 
+        trigger = 0;
+    }
+}
 
+function removeSelected(){
+    if(trigger === 1){
+        console.log(todolist.target.id);
+    }
 }
